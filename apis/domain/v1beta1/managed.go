741951ddff035fe6ce7225839651f2457c293587
Ross Golder <ross@golder.org>
2026-05-22 16:41:54 +0700

    Implement SetWriteConnectionSecretToReference for all resources

---
+++ apis/domain/v1beta1/managed.go
@@ -0,0 +1,5 @@
+package v1beta1
+import xpv2 "github.com/crossplane/crossplane/apis/v2/core/v2"
+func (in *Domain) SetWriteConnectionSecretToReference(r *xpv2.LocalSecretReference) {
+	in.Spec.WriteConnectionSecretToReference = r
+}

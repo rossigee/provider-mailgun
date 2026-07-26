624cb1afa02147aa9743abbaeefbd95c56b9646a
Ross Golder <ross@golder.org>
2026-07-26 14:16:14 +0700

    fix: add metav1.AddToGroupVersion to ProviderConfig types

--- apis/v1beta1/groupversion_info.go
+++ apis/v1beta1/groupversion_info.go
@@ -23,6 +23,7 @@ package v1beta1
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -43,5 +44,6 @@ func addKnownTypes(s *runtime.Scheme) error {
 		&ProviderConfigUsage{},
 		&ProviderConfigUsageList{},
 	)
+	metav1.AddToGroupVersion(s, SchemeGroupVersion)
 	return nil
 }
